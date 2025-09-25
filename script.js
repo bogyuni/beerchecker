befefec47076515a68f0c401c7a6d7c420d2f474
bogyuni <bogyuni@hotmail.com>
2025-09-25 13:31:26 +0900

    yyy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     const btn = document.querySelector('#resultAction');
     btn.addEventListener('click', function(){
-        alert(btn);
+       // alert(btn);
     });
     const winw = window.innerWidth;
     alert(winw);
