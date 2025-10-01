6a2f3569cb1fabbb7e40b6962085268b5963d737
bogyuni <bogyuni@hotmail.com>
2025-10-01 17:38:25 +0900

    result

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.onload = () => {
-    const btn = document.querySelector('#resultAction');
-    btn.addEventListener('click', function(){
-       // alert(btn);
+    const resultBtn = document.querySelector('#resultAction');
+    resultBtn.addEventListener('click', function(){
+       const ML = document.querySelector('#ML');
+       const QTY = document.querySelector('#QTY');
+       const WON = document.querySelector('#WON');
+       const standard = 500;
+       const resultWON = document.querySelector('#resultWON');
+       const oneCanToWon = WON / (ML * QTY / standard);
+       resultWON.innerHTML = oneCanToWon;
     });
-    const winw = window.innerWidth;
-    alert(winw);
+    //const winw = window.innerWidth;
+    //alert(winw);
 }
