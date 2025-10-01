7b26e12be941e1723eca87dbc64cecb8b365f6d3
bogyuni <bogyuni@hotmail.com>
2025-10-01 17:56:29 +0900

    12313

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     const resultBtn = document.querySelector('#resultAction');
     resultBtn.addEventListener('click', function(){
-       const ML = document.querySelector('#ML');
-       const QTY = document.querySelector('#QTY');
-       const WON = document.querySelector('#WON');
+       const ML = parseInt(document.querySelector('#ML').value);
+       const QTY = parseInt(document.querySelector('#QTY').value);
+       const WON = parseInt(document.querySelector('#WON').value);
        const standard = 500;
        const resultWON = document.querySelector('#resultWON');
        const oneCanToWon = WON / (ML * QTY / standard);
